1f3fd7b42bfb9b8efc715112095dc1828644c7b8
yuikoito <yuiko.dev@gmail.com>
2023-07-08 21:08:17 +0200

    mod:bug: add refs

--- src/javascript30/counter/index.ts
+++ src/javascript30/counter/index.ts
@@ -1,3 +1,6 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+// @refs https://leetcode.com/problems/create-hello-world-function/?envType=study-plan-v2&envId=30-days-of-javascript
+
 export const createCounter = (input: number) => {
   return () => input++;
 };
--- src/javascript30/helloWorld/index.ts
+++ src/javascript30/helloWorld/index.ts
@@ -1,3 +1,6 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+// @refs https://leetcode.com/problems/counter/?envType=study-plan-v2&envId=30-days-of-javascript
+
 export const createHelloWorld = () => {
   return (...args): "Hello World" => "Hello World";
 };
